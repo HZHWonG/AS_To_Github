29bf30aada89d59be95badfb22806357f0bc9f28
WonG <346623648@qq.com>
2018-07-06 00:50:35 +0800

    新增文件

---
+++ app/src/main/java/com/example/wong/git_test/Test_Git_add4.java
@@ -0,0 +1,18 @@
+package com.example.wong.git_test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * @author WonG
+ * @time 2018/7/1  12:12
+ * @desc ${TODD}
+ */
+public class Test_Git_add4 extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
